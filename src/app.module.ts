9d492f4edebba33b6465af67644c6872360e8824
akrabal <attikpoakrabal@gmail.com>
2023-11-21 11:25:53 +0000

    add .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ContactsModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(
-      'mongodb+srv://kimo:a7EmdtvmnHW5bvG8@cluster0.2nrdh.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: `mongodb+srv://${config.get('DATABASE_USER')}:${config.get(
+          'DATABASE_PASSWORD',
+        )}@cluster0.2nrdh.mongodb.net/?retryWrites=true&w=majority`,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
